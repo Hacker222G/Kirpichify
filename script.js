f33c8cada275b7b8425de16af84e627fcf2494d5
Hacker222G <hilehelpcomputer@gmail.com>
2025-04-19 18:18:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Функция воспроизведения
 function playTrack(audioUrl) {
-  const player = document.getElementById("player");
-  player.src = audioUrl;
-  player.play();
+  const player = document.querySelector(".player");
+  player.style.transform = "translateY(0)";
+  const audio = new Audio(audioUrl);
+  audio.play();
+  // Здесь можно добавить логику для обновления информации в плеере
 }
 
 // Обработчик клика по карточке
